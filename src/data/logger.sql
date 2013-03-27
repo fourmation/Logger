a94070c83269903b670f69d0f6dc6b406f06c3bd
Aaron Weatherall <aaron@4mation.com.au>
2013-03-28 10:05:58 +1100

    Updating sql file

--- src/data/logger.sql
+++ src/data/logger.sql
@@ -5,9 +5,10 @@ DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs` (
   `log_id` int(11) NOT NULL AUTO_INCREMENT,
   `comment` longtext COLLATE utf8_unicode_ci,
+  `comment_type` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `author` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link_id` int(11) DEFAULT NULL,
   `link_table` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `comment_type` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `link_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`log_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
